b7a677fc11180c4307c0556b5e4b03a28ebbc0eb
Davor <davor.komusanac@gmail.com>
2020-08-10 01:56:40 +0200

    Refactored to be compatible when the user finishes the project

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
 import 'package:one_thing/models/task.dart';
 import 'dart:collection';
+import 'package:one_thing/utils/constants.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> _tasks = [
-    Task(name: 'Buy milk'),
-  ];
+  Box tasksBox;
+  List<Task> _tasks;
+
+  TaskData() {
+    tasksBox = Hive.box(kTasksBoxName);
+    _tasks = Hive.box(kTasksBoxName).get('tasks').cast<Task>();
+  }
 
   UnmodifiableListView<Task> get tasks {
     return UnmodifiableListView(_tasks);
@@ -18,16 +24,25 @@ class TaskData extends ChangeNotifier {
   void addTask(String newTaskTitle) {
     final task = Task(name: newTaskTitle);
     _tasks.add(task);
+    tasksBox.put('tasks', _tasks);
     notifyListeners();
   }
 
   void updateTask(Task task) {
     task.toggleDone();
+    tasksBox.put('tasks', _tasks);
     notifyListeners();
   }
 
   void deleteTask(Task task) {
     _tasks.remove(task);
+    tasksBox.put('tasks', _tasks);
+    notifyListeners();
+  }
+
+  void deleteAllTasks() {
+    _tasks = [];
+    tasksBox.put('tasks', _tasks);
     notifyListeners();
   }
 
