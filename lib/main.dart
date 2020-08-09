37e8234afdfb175d55af0b569f6d2ddd910cb0cb
Davor <davor.komusanac@gmail.com>
2020-08-10 01:52:00 +0200

    Locked device orientation to only vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
+import 'package:one_thing/models/project_name_data.dart';
 import 'package:one_thing/models/task.dart';
 import 'package:one_thing/models/task_data.dart';
 import 'package:one_thing/screens/home_screen.dart';
@@ -10,22 +12,33 @@ import 'package:provider/provider.dart';
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(TaskAdapter());
-  await Hive.openBox(kBoxName);
+  var box = await Hive.openBox(kTasksBoxName);
+  var projectNameBox = await Hive.openBox(kProjectNameBox);
+  if (box.get('tasks') == null) {
+    box.put('tasks', []);
+  }
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return ChangeNotifierProvider(
-      create: (context) => TaskData(),
-      child: MaterialApp(
-        title: 'One Thing',
-        theme: ThemeData.light().copyWith(
-            appBarTheme: AppBarTheme(
-          color: Color(0xff283593),
-        )),
-        home: HomeScreen(),
+      create: (context) => ProjectNameData(),
+      child: ChangeNotifierProvider(
+        create: (context) => TaskData(),
+        child: MaterialApp(
+          title: 'One Thing',
+          theme: ThemeData.light().copyWith(
+              appBarTheme: AppBarTheme(
+            color: Color(0xff283593),
+          )),
+          home: HomeScreen(),
+        ),
       ),
     );
   }
