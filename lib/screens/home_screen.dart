cfa100a189168ee37196f67c2fb96a09b8cdd8cc
Davor <davor.komusanac@gmail.com>
2020-08-10 23:44:36 +0200

    refactor

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -85,10 +85,11 @@ class _HomeScreenState extends State<HomeScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.only(
-                top: 50.0,
-                left: 30.0,
-              ),
+              padding: EdgeInsets.only(
+                  //top: 50.0,
+                  //left: 30.0,
+                  top: MediaQuery.of(context).size.height / 20,
+                  left: 28.0),
               child: LogoCard(),
             ),
             Padding(
